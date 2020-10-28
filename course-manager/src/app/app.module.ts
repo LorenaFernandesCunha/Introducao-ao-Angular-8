308ca0310d09ca068f343089f99bbeb66e92d954
Lorena Fernandes Cunha <lfernacu@everis.com>
2020-10-28 15:12:31 -0300

    Criando Pasta Core - Componentes que não são mto utilizados

--- course-manager/src/app/app.module.ts
+++ course-manager/src/app/app.module.ts
@@ -1,8 +1,8 @@
+import { CoreModule } from './core/core.module';
 import { CourseModule } from './course/course.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { ErrorComponent } from './error/error.component';
@@ -10,7 +10,6 @@ import { ErrorComponent } from './error/error.component';
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent,
     ErrorComponent
 
   ],
@@ -18,6 +17,7 @@ import { ErrorComponent } from './error/error.component';
     BrowserModule,
     HttpClientModule,
     CourseModule,
+    CoreModule,
     RouterModule.forRoot([
       {path: '', redirectTo: 'courses', pathMatch: 'full'},
       {path: '**', component: ErrorComponent}
